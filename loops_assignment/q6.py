d2273cb5d759c90b4d41e59592a28c8ba4e67d93
Divayansu-Nayak-cg <divyansu.nayak.cg@gmail.com>
2026-09-10 09:33:14 +0530

    Add loops_assignment: 20 loop problems

---
+++ loops_assignment/q6.py
@@ -0,0 +1,4 @@
+num=int(input("enter a number:"))
+for i in range(1,21):
+    if i%2==0:
+        print(f"{i} is even number")
---
+++ loops_assignment/q7.py
@@ -0,0 +1,4 @@
+num=int(input("enter a number:"))
+for i in range(0,20):
+    if i%2==1:
+        print(f"{i} is a odd number")
